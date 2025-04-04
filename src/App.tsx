9bd928a6bd28540410353821c59055277212118b
Erdei Gergő János <erdei.gergo.janos@mechwart.local>
2025-04-04 12:20:53 +0200

    base work

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ const App = () => {
     {icon: "🏭", cookiePerSec: 100, price: 10},
   ])
 
+  const [pocket, setPocket] = useState<ItemType[]>([])
+
   const click = () => {
     setCookie(cookie + 1)
     localStorage.setItem("cookie", `${cookie + 1}`)
@@ -23,18 +25,37 @@ const App = () => {
       setCookie(Number(localStorage.getItem("cookie")))
     }
   }, [])
+  
+  const buyItem = (item: ItemType) =>{
+    setCookie(prev => prev - item.price)
+    setPocket(prev => [...prev, item])
+  }
+
+  useEffect(() =>{
+    const intervalId = setInterval(()=>{
+      pocket.forEach(item => {
+        setCookie(prev => {
+          localStorage.setItem("cookie", `${prev}`)
+          return prev + item.cookiePerSec / 10})
+      })
+
+    },100)
+
+    return () => clearInterval(intervalId)
+  }, [pocket])
 
   return (
     <div>
       <p>{cookie}</p>
       <button onClick={click}>🍪</button>
-
       {
         items.map(item =>
-        <button disabled={cookie < item.price}>
+        <button disabled={cookie < item.price} onClick={() => buyItem(item)}>
           {item.icon}
         </button>) 
       }
+      <p>Zseb: {pocket.map(item => <span>{item.icon}</span>)}</p>
+
     </div>
    )
 }
